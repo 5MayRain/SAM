0a3478bc4e35fcdecba00618c9432377ca78c6e5
5MayRain <zgh2606@gmail.com>
2026-02-02 11:17:44 +0800

    20260202

--- scripts/t.sh
+++
@@ -1,5 +0,0 @@
-grep 'block_ad' "NoAdsService.sh" | grep -o '".*"' | tr -d '"' | while IFS= read -r p
-do
-    echo $p
-    #[ -n "$p" ]&&[ -e "$p" ]&&find "$p" \( -type f -o -type d \) |while IFS= read -r f;do if [ -d "$f" ];then lsattr -d "$f"|grep -q "i-"&&{ chattr -i "$f";rmdir "$f";};else lsattr "$f"|grep -q "i-"&&{ chattr -i "$f";rm -f "$f";};fi;done;
-done
